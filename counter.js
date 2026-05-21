dfe9d364aca8aff30172fea69d0624bcfb8e044b
contactozuply <contactozuply@gmail.com>
2026-05-20 21:44:43 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,11 +1,15 @@
 // URL de tu Apps Script publicado
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyCcVxZTrrb_DN-zdoPNARvCS2vV0c72grUq0xVDN03FHjcYpjpwBna-rUF3sXtnNIPyQ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyjHy6i8yulAd0Y2BMFAlQhzg8c0laecyekXWaPo7ry_iEXeGe1XV3RAZ1Q8BlnSrvbcw/exec";
 
 // Función para cargar el contador al entrar
 function loadCounter() {
   const script = document.createElement("script");
   script.src = SCRIPT_URL + "?callback=updateCounter";
   document.body.appendChild(script);
+}
+
+function updateCounter(data) {
+  document.getElementById("cnt-1").textContent = data.count;
 }
 
 function updateCounter(data) {
