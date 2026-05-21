10407d92ec6ec61e7ca10fc09a5be6387518aad3
contactozuply <contactozuply@gmail.com>
2026-05-20 21:35:16 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,15 +1,15 @@
 // URL de tu Apps Script publicado
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxyJrC_Ymnxx4MVz0J4ekhRKdAdILRitiAkKCZQTlC4DjXcARFCN2lthOyhiviGcgq0Yw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyCcVxZTrrb_DN-zdoPNARvCS2vV0c72grUq0xVDN03FHjcYpjpwBna-rUF3sXtnNIPyQ/exec";
 
 // Función para cargar el contador al entrar
 function loadCounter() {
-  fetch(SCRIPT_URL)
-    .then(res => res.json())
-    .then(data => {
-      console.log("Respuesta del script (GET):", data); // 👈 aquí ves qué devuelve
-      document.getElementById("cnt-1").textContent = data.count;
-    })
-    .catch(err => console.error("Error cargando contador:", err));
+  const script = document.createElement("script");
+  script.src = SCRIPT_URL + "?callback=updateCounter";
+  document.body.appendChild(script);
+}
+
+function updateCounter(data) {
+  document.getElementById("cnt-1").textContent = data.count;
 }
 
 // Función para incrementar el contador al enviar formulario
