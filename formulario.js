6e2731c826d3293f4ddf2124db25e5afe9ea3ed2
contactozuply <contactozuply@gmail.com>
2026-06-23 22:40:17 -0400

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -110,8 +110,8 @@ document.addEventListener("DOMContentLoaded", () => {
       if (entries[0].isIntersecting) {
         loadCounter();
         cntObserver.disconnect();
-        // Polling discreto cada 20 segundos
-        setInterval(loadCounter, 20000);
+        // Polling discreto cada 10 segundos
+        setInterval(loadCounter, 10000);
       }
     });
     cntObserver.observe(cntEl);
